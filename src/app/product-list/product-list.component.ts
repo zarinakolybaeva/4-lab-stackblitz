d05220c2aeb187ab334e8263c96e0228e02498fa
zarinakolybaeva <114660048+zarinakolybaeva@users.noreply.github.com>
2023-02-18 15:23:09 +0600

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,13 +8,17 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
   products = products;
 
   share() {
     window.alert('The product has been shared!');
   }
-}
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
