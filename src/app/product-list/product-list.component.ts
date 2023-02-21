faaf05b18ca1c9bd513bc9c067e75e426837346e
zarinakolybaeva <114660048+zarinakolybaeva@users.noreply.github.com>
2023-02-21 08:09:33 +0600

    3 files modified, 5 deleted

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,9 +15,7 @@ export class ProductListComponent {
     window.alert('The product has been shared!');
   }
 
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
-  }
+ 
 }
 
 /*
